b73c3a792b50a1bd947241935caf7a66a31f4607
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-05-11 21:02:32 +0700

    Components Part 1
    Components Part 1

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <p className='footer'>copy right © 2022</p>
+            </div>
+        )
+    }
+}    
---
+++ src/components/Players.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+export default class player extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='assets/images/cr.jpg' class='image'/>
+                        <h3>Cristiano Ronaldo</h3>
+                        <p className='title'>Manchester United</p>
+                        <p><button type='button' class='block'>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='assets/images/kante.jpg'class='image' />
+                        <h3>Kante</h3>
+                        <p className='title'>Chelsea</p>
+                        <p><button type='button' class='block'>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='assets/images/messi.jpg' class='image'/>
+                        <h3>Messi</h3>
+                        <p className='title'>PSG</p>
+                        <p><button type='button' class='block'>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='assets/images/neymar.jpg'class='image' />
+                        <h3>Neymar</h3>
+                        <p className='title'>PSG</p>
+                        <p><button type='button' class='block'>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='assets/images/kane.jpg'class='image' />
+                        <h3>Kane</h3>
+                        <p className='title'>Tottemham</p>
+                        <p><button type='button' class='block'>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='assets/images/haaland.jpg'class='image' />
+                        <h3>Haaland</h3>
+                        <p className='title'>Manchester City</p>
+                        <p><button type='button' class='block'>Detail</button></p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
