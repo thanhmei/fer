c1399002b9f1453c583852f06ccdf5f33df88a90
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-05-15 22:18:22 +0700

    component 1 reup
    component 1 reup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,34 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 export default class Navigation extends Component{
     render(){
         return(
-            <div id="header">
-                <nav>
-                    <ul id="nav">
-                        <li><a className='active' href='#home'>Home</a></li>
-                        <li><a href='#news'>News</a></li>
-                        <li><a href='#about'>About</a></li>
-                        <li><a href='#contact'>Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
+          <div id="header">
+               <nav>
+             <ul id="nav">
+                 <li><a className='active' href='#home'>Home</a></li>
+                 <li><a href='#news'>News</a></li>
+                 <li><a href='#about'>About</a></li>
+                 <li><a href='#contact'>Contact</a></li>
+             </ul>
+           </nav>
+           </div>
+
         )
     }
 }
+
+// import React from 'react'
+// export default function Navigation() {
+//   return (
+//     <div id="header">
+//     <nav>
+//   <ul id="nav">
+//       <li><a className='active' href='#home'>Home</a></li>
+//       <li><a href='#news'>News</a></li>
+//       <li><a href='#about'>About</a></li>
+//       <li><a href='#contact'>Contact</a></li>
+//   </ul>
+// </nav>
+// </div>
+//   )
+// }
