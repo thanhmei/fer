a1242f0487a72dedc924b57ead63030b04b7af31
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-05-15 22:23:25 +0700

    Presentational and Container Components
    Presentational and Container Components

---
+++ New folder (3)/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navigation from './components/Navigation';
+import Player from './components/Players';
+import Footer from './components/Footer';
+function App(){
+    return(
+        <div className='App'>
+            <Navigation/>
+            <Player/>
+            <Footer/>
+        </div>
+    );
+}
+export default App;
+
+
