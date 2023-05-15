42f3a52a85c7af04df83a45367ee44bbda965776
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-05-15 22:05:30 +0700

    Revert "component 1 reup"
    This reverts commit 91150286961f362cececb333e3e3d9c2e7fd1b33.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,17 @@
-// import React, { Component } from 'react'
-// export default class Navigation extends Component{
-//     render(){
-//         return(
-//             <div>
-//                 <nav >
-//                     <ul class="navigator" >
-//                         <li  class="navigator_item" ><a className='active' href='#home'>Home</a></li>
-//                         <li class="navigator_item"><a href='#news'>News</a></li>
-//                         <li class="navigator_item"><a href='#about'>About</a></li>
-//                         <li class="navigator_item"><a href='#contact'>Contact</a></li>
-//                     </ul>
-//                 </nav>
-
-//             </div>
-
-//         )
-//     }
-// }
-import React from 'react'
-export default function Navigation() {
-  return (
-    <div id="header">
-    <nav>
-  <ul id="nav">
-      <li><a className='active' href='#home'>Home</a></li>
-      <li><a href='#news'>News</a></li>
-      <li><a href='#about'>About</a></li>
-      <li><a href='#contact'>Contact</a></li>
-  </ul>
-</nav>
-</div>
-  )
+import React,{Component} from 'react'
+export default class Navigation extends Component{
+    render(){
+        return(
+            <div id="header">
+                <nav>
+                    <ul id="nav">
+                        <li><a className='active' href='#home'>Home</a></li>
+                        <li><a href='#news'>News</a></li>
+                        <li><a href='#about'>About</a></li>
+                        <li><a href='#contact'>Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 }
